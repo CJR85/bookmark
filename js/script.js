0bb1c810ecf7db74dcdbdbb172aea3bc83480cdc
Calvin Roberts <roberts.calvin85@gmail.com>
2023-02-19 18:07:20 +0000

    Accordion, Newsletter, Footer & Mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const tabs = document.querySelectorAll('.tab'),
-  panels = document.querySelectorAll('.panel');
+  panels = document.querySelectorAll('.panel'),
+  btn = document.getElementById('menu-btn'),
+  menu = document.getElementById('menu'),
+  logo = document.getElementById('logo');
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.eventListener('click', onTabClick));
+
+// Hamburger button listener
+btn.addEventListener('click', navToggle);
 
 function onTabClick(e) {
   // Deactivate all tabs
@@ -24,4 +30,16 @@ function onTabClick(e) {
     .getElementById('panels')
     .getElementsByClassName(classString)[0]
     .classList.remove('hidden');
+}
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg');
+  }
 }
